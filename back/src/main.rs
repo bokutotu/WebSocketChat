8c45e48ba076e2d77c9aabbc2cc38ef582655333
bokutotu <mushin.hudoushin@gmail.com>
2023-04-14 18:00:02 +0900

    wsで送ったものをそのまま返すサーバーを作成した

--- back/src/main.rs
+++ back/src/main.rs
@@ -8,9 +8,7 @@ use axum::{
     Router,
 };
 use axum::extract::connect_info::ConnectInfo;
-use axum::extract::ws::CloseFrame;
 
-use std::borrow::Cow;
 use std::ops::ControlFlow;
 use std::net::SocketAddr;
 
@@ -18,11 +16,9 @@ use futures::{sink::SinkExt, stream::StreamExt};
 
 #[tokio::main]
 async fn main() {
-    // build our application with some routes
     let app = Router::new()
         .route("/ws", get(ws_handler));
 
-    // run it with hyper
     let addr = SocketAddr::from(([127, 0, 0, 1], 1919));
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
@@ -30,11 +26,7 @@ async fn main() {
         .unwrap();
 }
 
-/// The handler for the HTTP request (this gets called when the HTTP GET lands at the start
-/// of websocket negotiation). After this completes, the actual switching from HTTP to
-/// websocket protocol will occur.
-/// This is the last point where we can extract TCP/IP metadata such as IP address of the client
-/// as well as things from HTTP headers such as user-agent of the browser etc.
+
 async fn ws_handler(
     ws: WebSocketUpgrade,
     user_agent: Option<TypedHeader<headers::UserAgent>>,
@@ -46,27 +38,17 @@ async fn ws_handler(
         String::from("Unknown browser")
     };
     println!("`{user_agent}` at {addr} connected.");
-    // finalize the upgrade process by returning upgrade callback.
-    // we can customize the callback by sending additional info such as address.
     ws.on_upgrade(move |socket| handle_socket(socket, addr))
 }
 
-/// Actual websocket statemachine (one will be spawned per connection)
 async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
-    //send a ping (unsupported by some browsers) just to kick things off and get a response
     if socket.send(Message::Ping(vec![1, 2, 3])).await.is_ok() {
         println!("Pinged {}...", who);
     } else {
         println!("Could not send ping {}!", who);
-        // no Error here since the only thing we can do is to close the connection.
-        // If we can not send messages, there is no way to salvage the statemachine anyway.
         return;
     }
 
-    // receive single message from a client (we can either receive or send with socket).
-    // this will likely be the Pong for our Ping or a hello message from client.
-    // waiting for message from a client will block this task, but will not block other client's
-    // connections.
     if let Some(msg) = socket.recv().await {
         if let Ok(msg) = msg {
             if process_message(msg, who).is_break() {
@@ -78,87 +60,56 @@ async fn handle_socket(mut socket: WebSocket, who: SocketAddr) {
         }
     }
 
-    // Since each client gets individual statemachine, we can pause handling
-    // when necessary to wait for some external event (in this case illustrated by sleeping).
-    // Waiting for this client to finish getting its greetings does not prevent other clients from
-    // connecting to server and receiving their greetings.
-    for i in 1..5 {
-        if socket
-            .send(Message::Text(format!("Hi {i} times!")))
-            .await
-            .is_err()
-        {
-            println!("client {who} abruptly disconnected");
-            return;
-        }
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-    }
 
-    // By splitting socket we can send and receive at the same time. In this example we will send
-    // unsolicited messages to client based on some sort of server's internal event (i.e .timer).
     let (mut sender, mut receiver) = socket.split();
 
-    // Spawn a task that will push several messages to the client (does not matter what client does)
-    let mut send_task = tokio::spawn(async move {
-        let n_msg = 20;
-        for i in 0..n_msg {
-            // In case of any websocket error, we exit.
-            if sender
-                .send(Message::Text(format!("Server message {i} ...")))
-                .await
-                .is_err()
-            {
-                return i;
-            }
+    // resv_taskからsend_taskにメッセージを送信するためのチャネル
+    let (tx, mut rx) = tokio::sync::mpsc::channel(32);
 
-            tokio::time::sleep(std::time::Duration::from_millis(300)).await;
-        }
-
-        println!("Sending close to {who}...");
-        if let Err(e) = sender
-            .send(Message::Close(Some(CloseFrame {
-                code: axum::extract::ws::close_code::NORMAL,
-                reason: Cow::from("Goodbye"),
-            })))
-            .await
-        {
-            println!("Could not send Close due to {}, probably it is ok?", e);
-        }
-        n_msg
-    });
-
-    // This second task will receive messages from client and print them on server console
-    let mut recv_task = tokio::spawn(async move {
+    // メッセージを受け取るタスクを起動
+    let recv_task = tokio::spawn(async move {
         let mut cnt = 0;
         while let Some(Ok(msg)) = receiver.next().await {
             cnt += 1;
-            // print message and break if instructed to do so
-            if process_message(msg, who).is_break() {
+
+            if let Message::Text(text) = msg {
+                // メッセージを送信するタスクにメッセージを送信
+                println!("message: {:?} from : {:?}", text, who);
+                tx.send(text).await.unwrap();
+            } else if let Message::Close(_) = msg {
+                // Closeメッセージを受け取ったら、メッセージを送信するタスクにCloseメッセージを送信
+                println!("close: from : {:?}", who);
+                tx.send("close".to_string()).await.unwrap();
                 break;
             }
         }
         cnt
     });
+    
+    // メッセージを送信するタスクを起動
+    let send_task = tokio::spawn(async move {
+        while let Some(msg) = rx.recv().await {
+            if msg == "close" {
+                // メッセージを受け取るタスクからCloseメッセージを受け取ったら、Closeメッセージを送信して終了
+                sender.send(Message::Close(None)).await.unwrap();
+                break;
+            } else {
+                // メッセージを受け取るタスクからメッセージを受け取ったら、メッセージを送信
+                sender.send(Message::Text(msg)).await.unwrap();
+            }
+        }
+    });
 
-    // If any one of the tasks exit, abort the other.
+    // tokio::selct!を使って、メッセージを受け取るタスクとメッセージを送信するタスクのどちらかが終了するまで待つ
     tokio::select! {
-        rv_a = (&mut send_task) => {
-            match rv_a {
-                Ok(a) => println!("{} messages sent to {}", a, who),
-                Err(a) => println!("Error sending messages {:?}", a)
-            }
-            recv_task.abort();
-        },
-        rv_b = (&mut recv_task) => {
-            match rv_b {
-                Ok(b) => println!("Received {} messages", b),
-                Err(b) => println!("Error receiving messages {:?}", b)
-            }
-            send_task.abort();
+        _ = recv_task => {
+            println!("end resc_task");
+        }
+        _ = send_task => {
+            println!("{} sent a message", who);
         }
     }
 
-    // returning from the handler closes the websocket connection
     println!("Websocket context {} destroyed", who);
 }
 
--- front/src/pages/chat.tsx
+++ front/src/pages/chat.tsx
@@ -1,42 +1,46 @@
 import { useEffect, useState } from "react";
 
 export default function chat() {
-  
+  const [messages, setMessages] = useState<string[]>([]);
   const [socket, setSocket] = useState<WebSocket | null>(null);
 
   useEffect(() => {
     const s = new WebSocket("ws://localhost:1919/ws");
     setSocket(s);
   }, []);
-  // const socket = new WebSocket('wss://socketsbay.com/wss/v2/2/demo/');
+
   if (socket) {
-    socket.addEventListener('open', function (event) {
-      socket.send('Hello Server!');
-    });
-
     socket.addEventListener('message', function (event) {
-        console.log('Message from server ', event.data);
+      setMessages([...messages, event.data]);
+      console.log('Message from server ', event.data);
     });
-
-
-    setTimeout(() => {
-        const obj = { hello: "world" };
-        const blob = new Blob([JSON.stringify(obj, null, 2)], {
-          type: "application/json",
-        });
-        console.log("Sending blob over websocket");
-        socket.send(blob);
-    }, 1000);
-
-    setTimeout(() => {
-        socket.send('About done here...');
-        console.log("Sending close over websocket");
-        socket.close(3000, "Crash and Burn!");
-    }, 3000);
   }
+
+  const [ message, setMessage ] = useState<string>('');
+
   return (
     <div>
       chat
+      <input
+        type="text"
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+      />
+      <button
+        onClick={() => {
+          if (socket) {
+          socket.send(message);
+          }
+          setMessage('');
+        }}
+      >
+      送信
+      </button>
+
+      {messages.map((message) => (
+        <div>{message}</div>
+      ))}
+      
     </div>
   );
 }
